a30ac2bf13ef6cbe711b444b4b425fb0853e7500
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2024-05-10 20:20:28 +0500

    home, categories, lobbies (outline)

--- lib/views/categories.dart
+++ lib/views/categories.dart
@@ -1,12 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:qaida/components/all_button.dart';
+import 'package:qaida/components/place.dart';
+import 'package:qaida/components/search.dart';
 
 class Categories extends StatelessWidget {
   const Categories({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Категории'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Search(),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20.0),
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Популярные места',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+                AllButton(),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 180,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: const [
+                Place(),
+                Place(),
+                Place(),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Торгово развлекательные центры',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15.0,
+                  ),
+                ),
+                AllButton(),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 350,
+            child: GridView.count(
+              scrollDirection: Axis.horizontal,
+              crossAxisCount: 2,
+              children: [
+                for (int i = 0; i < 6; i++) const Place(),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Торгово развлекательные центры',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15.0,
+                  ),
+                ),
+                AllButton(),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 350,
+            child: GridView.count(
+              scrollDirection: Axis.horizontal,
+              crossAxisCount: 2,
+              children: [
+                for (int i = 0; i < 6; i++) const Place(),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/views/contacts.dart
+++ lib/views/contacts.dart
@@ -1,12 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:qaida/components/all_button.dart';
+import 'package:qaida/components/forward_button.dart';
+import 'package:qaida/components/search.dart';
 
 class Contacts extends StatelessWidget {
   const Contacts({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Конакты'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Search(),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20.0),
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Друзья',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+                AllButton(),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              for (int i = 0; i < 5; i++)
+                const ForwardButton(
+                  text: 'Примерный Пример',
+                  leading: Icon(
+                    Icons.person_pin,
+                    color: Color(0xFF1E3050),
+                    size: 50.0,
+                  ),
+                ),
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Группы',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+                AllButton(),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              for (int i = 0; i < 3; i++)
+                const ForwardButton(
+                  text: 'Примерный Пример',
+                  leading: Icon(
+                    Icons.square_rounded,
+                    color: Color(0xFF1E3050),
+                    size: 50.0,
+                  ),
+                ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
